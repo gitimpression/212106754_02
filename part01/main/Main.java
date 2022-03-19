56ab8b36ffccf187addbc3bae2c8a01236578bf8
gitimpression <2510852846@qq.com>
2022-03-20 00:16:44 +0800

    朴朴

--- part01/main/Main.java
+++ part01/main/Main.java
@@ -9,30 +9,33 @@ import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         String url = "https://j1.pupuapi.com/client/product/storeproduct/detail/deef1dd8-65ee-46bc-9e18-8cf1478a67e9/c2bb76bb-8663-4c64-bc49-185aa7bfe420";
+        Main main = new Main();
         // 展示商品所有信息
-        new Main().displayAll(url);
+        String goodsData = main.getGoodsData(url);
+        System.out.println(goodsData);
         // 展示实时价格(价格波动)
-        new Main().displayPrice(url, 3);
+        main.displayPrice(url, 3);
     }
     /**
-     * 功能：展示商品信息
+     * 功能：获取商品信息
      * @param url 商品地址
+     * @return 返回商品信息
      * */
-    public void displayAll(String url) throws IOException {
+    public String getGoodsData(String url) throws IOException {
+        StringBuilder sb = new StringBuilder();
         Main main = new Main();
         // 数据转json格式
         JSONObject jsonObject = JSONObject.parseObject(new Utils().sendGet(url));
-        System.out.println(jsonObject);
         // 得到data部分
         JSONObject data1 = jsonObject.getJSONObject("data");
-        // 输出关键信息
-        System.out.println("-----------------------商品：" + data1.get("name") + "-----------------------");
-        System.out.println("规格：" + data1.get("spec"));
-        System.out.println("价格：" + main.stringToDouble(data1.get("price").toString()));
-        System.out.println("折扣价/原价：" + main.stringToDouble(data1.get("price").toString()) + "/" + main.stringToDouble(data1.get("market_price").toString()));
-        System.out.println("-----------------------\"" + data1.get("name") + "\"的价格波动-----------------------");
+        // 添加关键信息 到 sb
+        sb.append("-----------------------商品：").append(data1.get("name")).append("-----------------------\n");
+        sb.append("规格：").append(data1.get("spec")).append("\n");
+        sb.append("价格：").append(main.stringToDouble(data1.get("price").toString())).append("\n");
+        sb.append("折扣价/原价：").append(main.stringToDouble(data1.get("price").toString())).append("/").append(main.stringToDouble(data1.get("market_price").toString())).append("\n");
+        sb.append("-----------------------\"").append(data1.get("name")).append("\"的价格波动-----------------------\n");
+        return sb.toString();
     }
     /**
      * 功能：获取商品价格
@@ -40,11 +43,8 @@ public class Main {
      * @return 双精度商品价格
      * */
     public double getPrice(String str){
-        // 转json
-        JSONObject dataObject = JSONObject.parseObject(str);
-        // 得到data部分
-        JSONObject data = dataObject.getJSONObject("data");
-        return Integer.parseInt(data.get("price").toString())*1.00/100.00;
+        // 转json 并 得到data部分
+        return Integer.parseInt(JSONObject.parseObject(str).getJSONObject("data").get("price").toString())*1.00/100.00;
     }
     /**
      * 功能：展示价格波动
