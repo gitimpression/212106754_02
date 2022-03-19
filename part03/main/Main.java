578e3faeb0136585e8d13dd10bc5cef10a20b84d
gitimpression <2510852846@qq.com>
2022-03-19 23:59:47 +0800

    拉钩

---
+++ part03/main/Main.java
@@ -0,0 +1,12 @@
+package part03.main;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("【选做】高阶：爬取拉勾网某个职位，某个公司规模，在某个城市的应届生薪资信息，并按\n" +
+                "照同等条件下，展示不同城市的应届生主要薪资范围分布情况。\n" +
+                "展示形式自定，可以使用图表展示，也可以直接控制台列出数据\n" +
+                "关于图表展示：可以使用echarts,excel等方式处理、展示数据\n" +
+                "示例：信息安全岗位在公司规模为150-500人的北京公司中的应届生的主要薪资范围为10k-15k，在同等公司规模的上海公司中应届生的主要薪资范围为12k-17k");
+    }
+}
